50d5657c607f9762e69c192cb17caec7bb88af4a
LorenzoLeonardini <lorenzo@leonardini.dev>
2022-05-04 09:17:08 +0200

    Improvement

--- src/examples/lib/NormalsMaterial.ts
+++ src/examples/lib/NormalsMaterial.ts
@@ -5,9 +5,9 @@ export class NormalsMaterial extends Material {
 		super()
 
 		this.fragmentSrc = `
-		vec3 normalizedNormals = (vNormal + vec3(1.0, 1.0, 1.0)) / 2.0;
+		vec3 color = (vNormal + vec3(1.0, 1.0, 1.0)) * 0.5;
 		`
-		this.gl_FragColor = 'vec4(normalizedNormals, 1.0)'
+		this.gl_FragColor = 'vec4(color, 1.0)'
 		super.create(gl)
 	}
 }
